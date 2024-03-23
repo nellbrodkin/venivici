9983315d20ed6895699c59b5ff80cff64747f517
Nell Brodkin <nellbrodkin@gmail.com>
2024-03-22 20:59:22 -0700

    finalized features

--- README.md
+++ README.md
@@ -1,8 +1,41 @@
-# React + Vite
+# Web Development Project 4 - *Rijksmuseum Art*
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+Submitted by: **Nell Brodkin**
 
-Currently, two official plugins are available:
+This web app: **Discover art from the rijksmuseum and customize what you see by banning artists or types of art**
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+Time spent: **5** hours spent in total
+
+## Required Features
+
+The following **required** functionality is completed:
+
+- [x] **Clicking a button creates a new API fetch request and displays at least three attributes from the returned JSON data**
+- [x] **Only one item/API call is viewable at a time**
+- [x] **API calls appear random to the user**
+- [x] **At least one image is displayed per API call**
+- [x] **Clicking on a displayed value for one attribute adds it to a displayed ban list**
+- [x] **Attributes on the ban list prevent further images/API results with that attribute from being displayed**
+
+The following **optional** features are implemented:
+
+- [x] Multiple types of attributes can be added to the ban list
+- [x] Users can see a stored history of their previously viewed items from their session
+
+## Video Walkthrough
+
+Here's a walkthrough of implemented features:
+
+<img src='rijksmuseumArtWalkthrough.gif' title='Video Walkthrough' width='' alt='Video Walkthrough' />
+
+<!-- Replace this with whatever GIF tool you used! -->
+GIF created with [Kap](https://getkap.co/) for macOS 
+<!-- Recommended tools:
+[Kap](https://getkap.co/) for macOS
+[ScreenToGif](https://www.screentogif.com/) for Windows
+[peek](https://github.com/phw/peek) for Linux. -->
+
+## Notes
+
+Future improvements would include better styling.
+Images come from https://data.rijksmuseum.nl/object-metadata/api/.
--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   }, []);
 
   const fetchData = async () => {
-    const randomPage = Math.floor(Math.random() * 100) + 1;
+    const randomPage = Math.floor(Math.random() * 100) + 1; // Assuming there are 100 pages of data
     const response = await fetch(`https://www.rijksmuseum.nl/api/en/collection?key=${ACCESS_KEY}&format=json&p=${randomPage}&ps=1`);
     const jsonData = await response.json();
 
@@ -24,10 +24,7 @@ function App() {
       if (detailsData && detailsData.artObject) {
         const objectTypes = detailsData.artObject.objectTypes.join(', ');
 
-        // define nationality
-        const nationality = detailsData.artObject.productionPlaces; 
-
-        const isBanned = banList.some(bannedAttribute => [artObject.title, artObject.principalOrFirstMaker, objectTypes, nationality].includes(bannedAttribute));
+        const isBanned = banList.some(bannedAttribute => [artObject.title, artObject.principalOrFirstMaker, objectTypes].includes(bannedAttribute));
 
         if (!isBanned) {
           setData({
@@ -35,7 +32,6 @@ function App() {
             image: artObject.webImage.url,
             description: artObject.principalOrFirstMaker,
             objectTypes: objectTypes,
-            nationality: nationality
           });
           setHistory([...history, {
             name: artObject.title,
@@ -48,6 +44,10 @@ function App() {
 
   const handleBan = (attributeValue) => {
     setBanList([...banList, attributeValue]);
+  };
+
+  const handleUnban = (attributeValue) => {
+    setBanList(banList.filter(item => item !== attributeValue));
   };
 
   return (
@@ -64,22 +64,30 @@ function App() {
         </ul>
       </div>
       <div className="data-container">
-        <button onClick={fetchData}>Fetch Data</button>
+        <h1>Rijksmuseum Art</h1>
+        <p>Discover art from the Rijksmuseum.
+          Click on painting attributes to ban them from future discoveries!</p>
         {data && (
           <div>
-            <h2>{data.name}</h2>
-            <img src={data.image} alt={data.name} />
-            <button className="attribute-button" onClick={() => handleBan(data.description)}>{data.description}</button>
-            <button className="attribute-button" onClick={() => handleBan(data.objectTypes)}>{data.objectTypes}</button>
-            <button className="attribute-button" onClick={() => handleBan(data.nationality)}>{data.nationality}</button> {/* Display the nationality attribute */}
+            <div className="image-description-container">
+              <h2>{data.name}</h2>
+              <img src={data.image} alt={data.name} />
+            </div>
+            <div className="attributes-container">
+              <button className="attribute-button" onClick={() => handleBan(data.description)}>{data.description}</button>
+              <button className="attribute-button" onClick={() => handleBan(data.objectTypes)}>{data.objectTypes}</button>
+            </div>
           </div>
         )}
+        <button onClick={fetchData}>Discover!</button>
       </div>
       <div className="ban-list">
         <h3>Ban List</h3>
         <ul>
           {banList.map((item, index) => (
-            <li key={index}>{item}</li>
+            <li key={index}>
+              <button className="attribute-button" onClick={() => handleUnban(item)}>{item}</button> {/* Clicking the button will remove the item from the ban list */}
+            </li>
           ))}
         </ul>
       </div>
